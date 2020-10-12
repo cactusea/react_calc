64d75fc3ae0f20c2d5ac0be61eda82873a990886
cactus <48524290+cactusea@users.noreply.github.com>
2020-10-12 14:51:00 +0900

    공학용 계산기 버튼 배열순서 변경

--- src/App.js
+++ src/App.js
@@ -37,40 +37,44 @@ const App = () => {
 const engnCalc = [
   ...basicCalc, 
   [
-    {id:'', text:'(', keytype:'top'},
-    {id:'', text:')', keytype:'top'},
-    {id:'', text:'mc', keytype:'top'},
-    {id:'', text:'m+', keytype:'top'},
-    {id:'', text:'m-', keytype:'top'},
-    {id:'', text:'mr', keytype:'top'},
-    {id:'', text:'2', super:'nd', keytype:'top'}, //클릭시 버튼 변경됨
-    {id:'', text:'x', super:'2', keytype:'top'},
-    {id:'', text:'x', super:'3', keytype:'top'},
-    {id:'', text:'x', super:'y', keytype:'top'},
-    {id:'', text:'y', super:'x', keytype:'top'},
-    {id:'', text:'2', super:'x', keytype:'top'},
-    {id:'', text:'⁄', preSuper: '1', sub:'x', keytype:'top'},
-    {id:'', text:'√', preSuper: '2', sub:'x', keytype:'top'},
-    {id:'', text:'√', preSuper: '3', sub:'x', keytype:'top'},
-    {id:'', text:'√', preSuper: 'y',sub:'x', keytype:'top'},
-    {id:'', text:'log', sub:'y', keytype:'top'},
-    {id:'', text:'log', sub:'2', keytype:'top'},
-    {id:'', text:'x!', keytype:'top'},
-    {id:'', text:'sin', super:'-1', keytype:'top'},
-    {id:'', text:'cos', super:'-1', keytype:'top'},
-    {id:'', text:'tan', super:'-1', keytype:'top'},
-    {id:'', text:'e', keytype:'top'},
-    {id:'', text:'EE', keytype:'top'},
-    {id:'', text:'Rad', keytype:'top'},
-    {id:'', text:'sinh', super:'-1', keytype:'top'},
-    {id:'', text:'cosh', super:'-1', keytype:'top'},
-    {id:'', text:'tanh', super:'-1', keytype:'top'},
-    {id:'', text:'π', keytype:'top'},
-    {id:'', text:'Rand', keytype:'top'},
+    {id:'24', text:'mr', keytype:'top'},
+    {id:'23', text:'m-', keytype:'top'},
+    {id:'22', text:'m+', keytype:'top'},
+    {id:'21', text:'mc', keytype:'top'},
+    {id:'20', text:')', keytype:'top'},
+    {id:'19', text:'(', keytype:'top'},
+
+    {id:'30', text:'2', super:'x', keytype:'top'},
+    {id:'29', text:'y', super:'x', keytype:'top'},
+    {id:'28', text:'x', super:'y', keytype:'top'},
+    {id:'27', text:'x', super:'3', keytype:'top'},
+    {id:'26', text:'x', super:'2', keytype:'top'},
+    {id:'25', text:'2', super:'nd', keytype:'top'}, //클릭시 버튼 변경됨
+
+    {id:'36', text:'log', sub:'2', keytype:'top'},
+    {id:'35', text:'log', sub:'y', keytype:'top'},
+    {id:'34', text:'√', preSuper: 'y',sub:'x', keytype:'top'},
+    {id:'33', text:'√', preSuper: '3', sub:'x', keytype:'top'},
+    {id:'32', text:'√', preSuper: '2', sub:'x', keytype:'top'},
+    {id:'31', text:'⁄', preSuper: '1', sub:'x', keytype:'top'},
+
+    {id:'43', text:'EE', keytype:'top'},
+    {id:'42', text:'e', keytype:'top'},
+    {id:'41', text:'tan', super:'-1', keytype:'top'},
+    {id:'39', text:'cos', super:'-1', keytype:'top'},
+    {id:'38', text:'sin', super:'-1', keytype:'top'},
+    {id:'37', text:'x!', keytype:'top'},
+
+    {id:'49', text:'Rand', keytype:'top'},
+    {id:'48', text:'π', keytype:'top'},
+    {id:'47', text:'tanh', super:'-1', keytype:'top'},
+    {id:'46', text:'cosh', super:'-1', keytype:'top'},
+    {id:'45', text:'sinh', super:'-1', keytype:'top'},
+    {id:'44', text:'Rad', keytype:'top'},
   ],
 ];
 
-const progCalc = [];
+  const progCalc = []; //프로그래머용
 
   const keypadType = [basicCalc, engnCalc, progCalc];
   const [typeKeypads, setTypeKeypads] = useState(keypadType[0]);
