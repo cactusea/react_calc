77aab8283c173387ad2f6595990eab76f140a45e
cactus <48524290+cactusea@users.noreply.github.com>
2020-10-15 16:05:48 +0900

    수식입력내역 파일 추가 및 css 적용

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './HistoryListStyle.scss';
 
 const HistoryList = ({mathHistory}) => {
   return(
     <div>
       <div className="mathHistory">
-        {mathHistory.map(history=> (
-            <div key={history.id}>
-              <span>{history.mathEx}</span>
-            </div>
-        ))}
+        <ul>
+          {mathHistory.map(history=> (
+            <li key={history.id}>{history.mathEx}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
